e1e715761113c13db4bfa71234c641feea3575ef
rl1987 <rl1987@users.noreply.github.com>
2021-07-07 17:11:23 +0300

    Start defining data model for scraping task

--- main.go
+++ main.go
@@ -7,6 +7,28 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
+type Vertex struct {
+	Type        int
+	Name        string
+	XPath       string
+	Regex       string
+	CSSSelector string
+	ItemName    string
+	FieldName   string
+}
+
+type Edge struct {
+	Name       string
+	FromVertex string
+	ToVertex   string
+}
+
+type SpiderTask struct {
+	Name     string
+	Vertices []Vertex
+	Edges    []Edge
+}
+
 func main() {
 	fmt.Println("SpiderSwarm")
 	httpClient := &http.Client{}
