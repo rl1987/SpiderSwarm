20e57eaa9dc52fc3da1be396d9d6b1425af10374
rl1987 <rl1987@users.noreply.github.com>
2021-07-12 15:57:58 +0300

    Start working on implementing Workflow

--- main.go
+++ main.go
@@ -5,29 +5,9 @@ import (
 	"net/http"
 
 	"github.com/davecgh/go-spew/spew"
+
+	"github.com/rl1987/SpiderSwarm/workflow"
 )
-
-type Vertex struct {
-	Type        int
-	Name        string
-	XPath       string
-	Regex       string
-	CSSSelector string
-	ItemName    string
-	FieldName   string
-}
-
-type Edge struct {
-	Name       string
-	FromVertex string
-	ToVertex   string
-}
-
-type SpiderTask struct {
-	Name     string
-	Vertices []Vertex
-	Edges    []Edge
-}
 
 func main() {
 	fmt.Println("SpiderSwarm")
---
+++ workflow/go.mod
@@ -0,0 +1,9 @@
+module github.com/rl1987/SpiderSwarm/workflow
+
+go 1.15
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/lestrrat-go/libxml2 v0.0.0-20201123224832-e6d9de61b80d // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
