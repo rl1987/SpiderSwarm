1278b228389756ed51f7a74bdbf53c749dd15de6
rl1987 <rl1987@users.noreply.github.com>
2021-07-13 14:54:18 +0300

    Flatten all the code into single file for time being

--- main.go
+++ main.go
@@ -5,12 +5,44 @@ import (
 	"net/http"
 
 	"github.com/davecgh/go-spew/spew"
-
-	"github.com/rl1987/SpiderSwarm/workflow"
 )
+
+type DataPipe struct {
+	Done  bool
+	Queue []interface{}
+}
+
+type AbstractAction struct {
+	Inputs     map[string]DataPipe
+	Outputs    map[string]DataPipe
+	CanFail    bool
+	ExpectMany bool
+}
+
+type Action interface {
+	Run() bool
+}
+
+type HTTPAction struct {
+	AbstractAction
+}
+
+type Task struct {
+	Inputs  map[string]DataPipe
+	Outputs map[string]DataPipe
+}
+
+type Workflow struct {
+	Name    string
+	Version string
+	Tasks   []Task
+}
 
 func main() {
 	fmt.Println("SpiderSwarm")
 	httpClient := &http.Client{}
 	spew.Dump(httpClient)
+
+	w := Workflow{}
+	spew.Dump(w)
 }
