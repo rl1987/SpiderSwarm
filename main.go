76ac5e7ac41dd8b942102ab7956cea9e2a3dab73
rl1987 <rl1987@users.noreply.github.com>
2021-07-14 16:12:54 +0300

    Build more foundations

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -12,16 +13,32 @@ type DataPipe struct {
 	Queue []interface{}
 }
 
+func NewDataPipe() *DataPipe {
+	return &DataPipe{false, []interface{}{}}
+}
+
 type AbstractAction struct {
-	Inputs     map[string]DataPipe
-	Outputs    map[string]DataPipe
-	CanFail    bool
-	ExpectMany bool
+	Inputs             map[string]*DataPipe
+	Outputs            map[string]*DataPipe
+	CanFail            bool
+	ExpectMany         bool
+	AllowedInputNames  []string
+	AllowedOutputNames []string
 }
 
 type Action interface {
 	Run() bool
+	AddInput(name string, dataPipe DataPipe)
+	AddOutput(name string, dataPipe DataPipe)
 }
+
+const HTTPActionInputURLParams = "HTTPActionInputURLParams"
+const HTTPActionInputHeaders = "HTTPActionInputHeaders"
+const HTTPActionInputCookies = "HTTPActionInputCookies"
+
+const HTTPActionOutputBody = "HTTPActionOutputBody"
+const HTTPActionOutputHeaders = "HTTPActionOutputHeaders"
+const HTTPActionOuputStatusCode = "HTTPActionOuputStatusCode"
 
 type HTTPAction struct {
 	AbstractAction
@@ -34,10 +51,45 @@ func NewHTTPAction(baseURL string, method string, canFail bool) *HTTPAction {
 		AbstractAction: AbstractAction{
 			CanFail:    canFail,
 			ExpectMany: false,
+			AllowedInputNames: []string{
+				HTTPActionInputURLParams,
+				HTTPActionInputHeaders,
+				HTTPActionInputCookies,
+			},
+			AllowedOutputNames: []string{
+				HTTPActionOutputBody,
+				HTTPActionOutputHeaders,
+				HTTPActionOuputStatusCode,
+			},
+			Inputs:  map[string]*DataPipe{},
+			Outputs: map[string]*DataPipe{},
 		},
 		BaseURL: baseURL,
 		Method:  method,
 	}
+}
+
+func (a *AbstractAction) AddInput(name string, dataPipe *DataPipe) error {
+	for _, n := range a.AllowedInputNames {
+		if n == name {
+			a.Inputs[name] = dataPipe
+			return nil
+		}
+	}
+
+	return errors.New("input name not in AllowedInputNames")
+}
+
+func (a *AbstractAction) AddOutput(name string, dataPipe *DataPipe) error {
+	for _, n := range a.AllowedOutputNames {
+		if n == name {
+			a.Outputs[name] = dataPipe
+			return nil
+		}
+	}
+
+	return errors.New("input name not in AllowedOutputNames")
+
 }
 
 type Task struct {
--- main_test.go
+++ main_test.go
@@ -16,4 +16,46 @@ func TestNewHTTPAction(t *testing.T) {
 	assert.False(t, httpAction.AbstractAction.ExpectMany)
 	assert.Equal(t, httpAction.BaseURL, baseURL)
 	assert.Equal(t, httpAction.Method, method)
+	assert.Equal(t, len(httpAction.AbstractAction.AllowedInputNames), 3)
+	assert.Equal(t, httpAction.AbstractAction.AllowedInputNames[0], HTTPActionInputURLParams)
+	assert.Equal(t, httpAction.AbstractAction.AllowedInputNames[1], HTTPActionInputHeaders)
+	assert.Equal(t, httpAction.AbstractAction.AllowedInputNames[2], HTTPActionInputCookies)
+	assert.Equal(t, len(httpAction.AbstractAction.AllowedOutputNames), 3)
+	assert.Equal(t, httpAction.AbstractAction.AllowedOutputNames[0], HTTPActionOutputBody)
+	assert.Equal(t, httpAction.AbstractAction.AllowedOutputNames[1], HTTPActionOutputHeaders)
+	assert.Equal(t, httpAction.AbstractAction.AllowedOutputNames[2], HTTPActionOuputStatusCode)
+
+}
+
+func TestAddInput(t *testing.T) {
+	baseURL := "https://httpbin.org/post"
+	method := "POST"
+
+	httpAction := NewHTTPAction(baseURL, method, false)
+
+	dp := NewDataPipe()
+
+	err := httpAction.AddInput("bad_name", dp)
+	assert.NotNil(t, err)
+
+	err = httpAction.AddInput(HTTPActionInputURLParams, dp)
+	assert.Nil(t, err)
+	assert.Equal(t, httpAction.AbstractAction.Inputs[HTTPActionInputURLParams], dp)
+
+}
+
+func TestAddOutput(t *testing.T) {
+	baseURL := "https://httpbin.org/post"
+	method := "POST"
+
+	httpAction := NewHTTPAction(baseURL, method, false)
+
+	dp := NewDataPipe()
+
+	err := httpAction.AddOutput("bad_name", dp)
+	assert.NotNil(t, err)
+
+	err = httpAction.AddOutput(HTTPActionOutputBody, dp)
+	assert.Nil(t, err)
+	assert.Equal(t, httpAction.AbstractAction.Outputs[HTTPActionOutputBody], dp)
 }
