ebc48289df67b9737014ef337c236b9d44e2e699
rl1987 <rl1987@users.noreply.github.com>
2021-07-25 16:14:45 +0300

    Move example task code to ExampleBasicTask() in main_test.go

--- main.go
+++ main.go
@@ -2,41 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	fmt.Println("SpiderSwarm")
-	httpAction := NewHTTPAction("https://cryptome.org", "GET", true)
-
-	headers := map[string][]string{
-		"User-Agent": []string{"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"},
-	}
-
-	headersIn := NewDataPipe()
-	resultOut := NewDataPipe()
-
-	httpAction.AddInput(HTTPActionInputHeaders, headersIn)
-	xpathAction := NewXPathAction("//a/text()", true)
-
-	task := NewTask("task1", "", "")
-	task.AddAction(httpAction)
-	task.AddAction(xpathAction)
-
-	task.AddInput("headersIn", httpAction, HTTPActionInputHeaders, headersIn)
-	task.AddOutput("resultOut", xpathAction, XPathActionOutputStr, resultOut)
-
-	task.AddDataPipeBetweenActions(httpAction, HTTPActionOutputBody, xpathAction, XPathActionInputHTMLBytes)
-
-	headersIn.Add(headers)
-
-	spew.Dump(task)
-
-	err := task.Run()
-	if err != nil {
-		spew.Dump(err)
-	} else {
-		spew.Dump(resultOut)
-	}
 }
--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -496,4 +497,39 @@ func TestFieldJoinActionRun(t *testing.T) {
 	assert.True(t, ok)
 
 	assert.Equal(t, expectedItem, item)
+}
+
+func ExampleBasicTask() {
+	httpAction := NewHTTPAction("https://cryptome.org", "GET", true)
+
+	headers := map[string][]string{
+		"User-Agent": []string{"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"},
+	}
+
+	headersIn := NewDataPipe()
+	resultOut := NewDataPipe()
+
+	httpAction.AddInput(HTTPActionInputHeaders, headersIn)
+	xpathAction := NewXPathAction("//a/text()", true)
+
+	task := NewTask("task1", "", "")
+	task.AddAction(httpAction)
+	task.AddAction(xpathAction)
+
+	task.AddInput("headersIn", httpAction, HTTPActionInputHeaders, headersIn)
+	task.AddOutput("resultOut", xpathAction, XPathActionOutputStr, resultOut)
+
+	task.AddDataPipeBetweenActions(httpAction, HTTPActionOutputBody, xpathAction, XPathActionInputHTMLBytes)
+
+	headersIn.Add(headers)
+
+	spew.Dump(task)
+
+	err := task.Run()
+	if err != nil {
+		spew.Dump(err)
+	} else {
+		spew.Dump(resultOut)
+	}
+
 }
