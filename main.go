e0d8b4b83419ae5837cd547ecd8dac1fcc4c765d
rl1987 <rl1987@users.noreply.github.com>
2021-07-25 16:43:37 +0300

    Construct and run one more example task

--- main.go
+++ main.go
@@ -2,8 +2,59 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	fmt.Println("SpiderSwarm")
+	fmt.Println("spiderswarm")
+
+	httpAction := NewHTTPAction("https://news.ycombinator.com/news", "GET", false)
+
+	titleXpathAction := NewXPathAction("//a[@class='storylink']/text()", true)
+	linkXpathAction := NewXPathAction("//a[@class='storylink']/@href", true)
+
+	task := NewTask("HN", "", "")
+
+	titlesOut := NewDataPipe()
+	linksOut := NewDataPipe()
+
+	task.AddAction(httpAction)
+	task.AddAction(titleXpathAction)
+	task.AddAction(linkXpathAction)
+
+	task.AddOutput("titles", titleXpathAction, XPathActionOutputStr, titlesOut)
+	task.AddOutput("links", linkXpathAction, XPathActionOutputStr, linksOut)
+
+	task.AddDataPipeBetweenActions(httpAction, HTTPActionOutputBody, titleXpathAction, XPathActionInputHTMLBytes)
+	task.AddDataPipeBetweenActions(httpAction, HTTPActionOutputBody, linkXpathAction, XPathActionInputHTMLBytes)
+
+	err := task.Run()
+	if err != nil {
+		spew.Dump(err)
+		return
+	}
+
+	var titles []string
+	var links []string
+
+	for {
+		if title, ok := titlesOut.Remove().(string); ok {
+			titles = append(titles, title)
+		} else {
+			break
+		}
+	}
+
+	for {
+		if link, ok := linksOut.Remove().(string); ok {
+			links = append(links, link)
+		} else {
+			break
+		}
+	}
+
+	spew.Dump(titles)
+	spew.Dump(links)
+
 }
