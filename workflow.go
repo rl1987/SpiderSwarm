a505555af1e3977fa54496d86e34dbb9e49dc752
rl1987 <rl1987@users.noreply.github.com>
2021-07-26 19:24:26 +0300

    More work on defining data model for Workflow

--- workflow.go
+++ workflow.go
@@ -1,8 +1,25 @@
 package main
 
+type ActionTemplate struct {
+	Name              string
+	StructName        string
+	ConstructorParams map[string]interface{}
+}
+
+type DataPipeTemplate struct {
+	SourceActionName string
+	DestActionName   string
+}
+
+type TaskTemplate struct {
+	TaskName          string
+	ActionTemplates   []ActionTemplate
+	DataPipeTemplates []DataPipeTemplate
+}
+
 type Workflow struct {
-	Name    string
-	Version string
-	UUID    string
-	Tasks   []Task // XXX: task templates?
+	Name          string
+	Version       string
+	UUID          string
+	TaskTemplates []TaskTemplate
 }
