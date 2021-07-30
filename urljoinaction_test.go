417539f7dea03de8011a067a13b1820cbb62fe4a
rl1987 <rl1987@users.noreply.github.com>
2021-07-30 12:39:06 +0300

    One more unit-test for URLJoinAction

--- urljoinaction_test.go
+++ urljoinaction_test.go
@@ -30,3 +30,34 @@ func TestURLJoinActionRun(t *testing.T) {
 
 	assert.Equal(t, absoluteURLStr, gotAbsoluteURLStr)
 }
+
+func TestURLJoinActionBaseWithRelative(t *testing.T) {
+	action := NewURLJoinAction("http://example.com/")
+
+	baseURLStr := "http://example.com/directory"
+	relativeURLStr := "../../..//search?q=golang"
+	absoluteURLStr := "http://example.com/search?q=golang"
+
+	baseIn := NewDataPipe()
+	relativeIn := NewDataPipe()
+	absoluteOut := NewDataPipe()
+
+	baseIn.Add(baseURLStr)
+	relativeIn.Add(relativeURLStr)
+
+	err := action.AddInput(URLJoinActionInputBaseURL, baseIn)
+	assert.Nil(t, err)
+
+	err = action.AddInput(URLJoinActionInputRelativeURL, relativeIn)
+	assert.Nil(t, err)
+
+	err = action.AddOutput(URLJoinActionOutputAbsoluteURL, absoluteOut)
+	assert.Nil(t, err)
+
+	err = action.Run()
+	assert.Nil(t, err)
+
+	gotAbsoluteURLStr, ok := absoluteOut.Remove().(string)
+	assert.True(t, ok)
+	assert.Equal(t, absoluteURLStr, gotAbsoluteURLStr)
+}
