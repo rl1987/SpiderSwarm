d9bca49aa75e056bd94aee80575430ea772fc330
rl1987 <rl1987@users.noreply.github.com>
2021-08-01 18:09:13 +0300

    Preliminary code to splay a TaskPromise

--- taskpromise.go
+++ taskpromise.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jinzhu/copier"
 )
 
 type TaskPromise struct {
@@ -45,4 +46,52 @@ func (tp *TaskPromise) IsSplayable() bool {
 	}
 
 	return hasLists && equalLen && lastLen != 0
+}
+
+func (tp *TaskPromise) splayOff() *TaskPromise {
+	newPromise := &TaskPromise{
+		UUID:                       uuid.New().String(),
+		TaskName:                   tp.TaskName,
+		WorkflowName:               tp.WorkflowName,
+		JobUUID:                    tp.JobUUID,
+		InputDataChunksByInputName: map[string]*DataChunk{},
+		CreatedAt:                  time.Now(),
+	}
+
+	for name, chunk := range tp.InputDataChunksByInputName {
+		if chunk.Type == DataChunkTypeStrings {
+			var s string
+			s, chunk.Payload = chunk.Payload.([]string)[0], chunk.Payload.([]string)[1:]
+			newChunk, _ := NewDataChunk(s)
+			newPromise.InputDataChunksByInputName[name] = newChunk
+		} else {
+			newPromise.InputDataChunksByInputName[name] = chunk
+		}
+	}
+
+	return newPromise
+}
+
+func (tp *TaskPromise) Splay() []*TaskPromise {
+	if !tp.IsSplayable() {
+		return []*TaskPromise{tp}
+	}
+
+	promises := []*TaskPromise{}
+
+	promiseCopy := &TaskPromise{}
+
+	copier.Copy(promiseCopy, tp)
+
+	for {
+		if !tp.IsSplayable() {
+			break
+		}
+
+		newPromise := promiseCopy.splayOff()
+
+		promises = append(promises, newPromise)
+	}
+
+	return promises
 }
