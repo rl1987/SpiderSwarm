9ab1ead45d7bdc4bc214dc2facf1b187523ac159
rl1987 <rl1987@users.noreply.github.com>
2021-08-03 19:22:18 +0300

    Write unit-test for an error condition in DataPipe.Add()

--- datapipe_test.go
+++ datapipe_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +17,15 @@ func TestNewDataPipeBetweenAction(t *testing.T) {
 	assert.Equal(t, httpAction, dataPipe.FromAction)
 	assert.Equal(t, xpathAction, dataPipe.ToAction)
 	assert.NotNil(t, dataPipe.Queue)
+}
+
+func TestDataPipeAddUnsupported(t *testing.T) {
+	err := errors.New("Unsupported payload type")
+
+	dataPipe := NewDataPipe()
+
+	gotErr := dataPipe.Add(err)
+
+	assert.NotNil(t, gotErr)
+	assert.Equal(t, err, gotErr)
 }
