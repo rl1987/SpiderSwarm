cce9c0c08aa450e7f22b3a99d9699b483a557ff1
rl1987 <rl1987@users.noreply.github.com>
2021-08-05 10:46:32 +0300

    Introduce action that outputs a constant value

---
+++ constaction.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+const ConstActionOutput = "ConstActionOutput"
+
+type ConstAction struct {
+	AbstractAction
+	C interface{}
+}
+
+func NewConstAction(c interface{}) *ConstAction {
+	return &ConstAction{
+		AbstractAction: AbstractAction{
+			CanFail:            false,
+			ExpectMany:         false,
+			AllowedInputNames:  []string{},
+			AllowedOutputNames: []string{ConstActionOutput},
+			Inputs:             map[string]*DataPipe{},
+			Outputs: map[string][]*DataPipe{
+				XPathActionOutputStr: []*DataPipe{},
+			},
+			UUID: uuid.New().String(),
+		},
+		C: c,
+	}
+}
+
+func (ca *ConstAction) Run() error {
+	if ca.Outputs[ConstActionOutput] == nil {
+		return errors.New("Output not connected")
+	}
+
+	for _, output := range ca.Outputs[ConstActionOutput] {
+		output.Add(ca.C)
+	}
+
+	return nil
+}
---
+++ constaction_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstActionRun(t *testing.T) {
+	c := "A flower raised in a greenhouse is still beautiful, even though it knows no adversity. But a flower growing in the field that has braved wind, rain, cold, and heat possesses something more than just beauty."
+
+	action := NewConstAction(c)
+
+	dataOut := NewDataPipe()
+
+	err := action.AddOutput(ConstActionOutput, dataOut)
+	assert.Nil(t, err)
+
+	err = action.Run()
+	assert.Nil(t, err)
+
+	gotC, ok := dataOut.Remove().(string)
+	assert.True(t, ok)
+	assert.Equal(t, c, gotC)
+}
