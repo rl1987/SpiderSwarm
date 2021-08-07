1388b405b47aa40ba13accc66c397fa7474b5941
rl1987 <rl1987@users.noreply.github.com>
2021-08-07 19:55:07 +0300

    Happy-path unit-test for StringCutAction.Run()

--- lib/stringcutoutaction_test.go
+++ lib/stringcutoutaction_test.go
@@ -23,3 +23,28 @@ func TestNewStringCutActionFromTemplate(t *testing.T) {
 	assert.Equal(t, "here", action.From)
 	assert.Equal(t, "eternity", action.To)
 }
+
+func TestStringCutActionRun(t *testing.T) {
+	inputStr := "... latitude: '12.222';"
+	expectedOutStr := "12.222"
+
+	action := NewStringCutAction("latitude: '", "'")
+
+	strIn := NewDataPipe()
+	strOut := NewDataPipe()
+
+	strIn.Add(inputStr)
+
+	err := action.AddInput(StringCutActionInputStr, strIn)
+	assert.Nil(t, err)
+
+	err = action.AddOutput(StringCutActionOutputStr, strOut)
+	assert.Nil(t, err)
+
+	err = action.Run()
+	assert.Nil(t, err)
+
+	gotStr, ok := action.Outputs[StringCutActionOutputStr][0].Remove().(string)
+	assert.True(t, ok)
+	assert.Equal(t, expectedOutStr, gotStr)
+}
