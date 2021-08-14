c5c7bb35703e50ba9401f6d663df1880a096f58a
rl1987 <rl1987@users.noreply.github.com>
2021-08-14 19:01:59 +0300

    Start working on exporter part

--- lib/exporter.go
+++ lib/exporter.go
@@ -1,16 +1,39 @@
 package spiderswarm
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
 type Exporter struct {
 	UUID     string
 	Backends []ExporterBackend
+	ItemsIn  chan Item
+}
+
+func NewExporter() *Exporter {
+	return &Exporter{
+		UUID:     uuid.New().String(),
+		Backends: []ExporterBackend{},
+		ItemsIn:  make(chan Item),
+	}
 }
 
 func (e *Exporter) Run() error {
-	for {
+	for item := range e.ItemsIn {
 		// Receive items, pass them to exporter backend(s).
+		log.Info(fmt.Printf("Exported %s got item %v", e.UUID, item))
 
-		break
+		for _, backend := range e.Backends {
+			backend.WriteItem(&item)
+		}
 	}
 
 	return nil
+}
+
+func (e *Exporter) AddBackend(newBackend ExporterBackend) {
+	e.Backends = append(e.Backends, newBackend)
 }
--- lib/exporterbackend.go
+++ lib/exporterbackend.go
@@ -1,7 +1,9 @@
 package spiderswarm
 
 type ExporterBackend interface {
-	// TODO
+	StartExporting(jobUUID string)
+	WriteItem(i *Item) error
+	FinishExporting(jobUUID string)
 }
 
 type AbstractExporterBackend struct {
