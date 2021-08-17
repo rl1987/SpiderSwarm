8d45c75fb1afad3f6a3c403828bc2389d4b53d4a
rl1987 <rl1987@users.noreply.github.com>
2021-08-17 16:15:12 +0300

    Preliminary implementation for HTTPCookieJoinAction.Run()

--- lib/httpcookiejoinaction.go
+++ lib/httpcookiejoinaction.go
@@ -1,6 +1,8 @@
 package spiderswarm
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
@@ -32,6 +34,37 @@ func NewHTTPCookieJoinAction() *HTTPCookieJoinAction {
 }
 
 func (hcja *HTTPCookieJoinAction) Run() error {
-	// TODO: implement
+	if hcja.Inputs[HTTPCookieJoinActionInputOldCookies] == nil || hcja.Inputs[HTTPCookieJoinActionInputNewCookies] == nil {
+		return errors.New("Both inputs must be connected")
+	}
+
+	if hcja.Outputs[HTTPCookieJoinActionOutputUpdatedCookies] == nil || len(hcja.Outputs[HTTPCookieJoinActionOutputUpdatedCookies]) == 0 {
+		return errors.New("Output not connected")
+	}
+
+	updatedCookies := map[string]string{}
+
+	oldCookies, ok1 := hcja.Inputs[HTTPCookieJoinActionInputOldCookies].Remove().(map[string]string)
+	if !ok1 {
+		return errors.New("Failed to get old cookies")
+	}
+
+	newCookies, ok2 := hcja.Inputs[HTTPCookieJoinActionInputNewCookies].Remove().(map[string]string)
+	if !ok2 {
+		return errors.New("Failed to get new cookies")
+	}
+
+	for key, value := range oldCookies {
+		updatedCookies[key] = value
+	}
+
+	for key, value := range newCookies {
+		updatedCookies[key] = value
+	}
+
+	for _, output := range hcja.Outputs[HTTPCookieJoinActionOutputUpdatedCookies] {
+		output.Add(updatedCookies)
+	}
+
 	return nil
 }
