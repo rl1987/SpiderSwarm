423b49a1aeddba8674f635b68b7a7446c818da7e
rl1987 <rl1987@users.noreply.github.com>
2021-08-18 16:08:26 +0300

    Write happy-path unit-test for HTTPCookieJoinAction.Run()

---
+++ lib/httpcookiejoinaction_test.go
@@ -0,0 +1,47 @@
+package spiderswarm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPCookieJoinActionRun(t *testing.T) {
+	oldCookies := map[string]string{
+		"session": "12211",
+		"n":       "1",
+	}
+
+	newCookies := map[string]string{
+		"n": "2",
+		"x": "1.11",
+	}
+
+	updatedCookies := map[string]string{
+		"session": "12211",
+		"n":       "2",
+		"x":       "1.11",
+	}
+
+	oldCookiesIn := NewDataPipe()
+	newCookiesIn := NewDataPipe()
+	updatedCookiesOut := NewDataPipe()
+
+	oldCookiesIn.Add(oldCookies)
+	newCookiesIn.Add(newCookies)
+
+	action := NewHTTPCookieJoinAction()
+
+	action.AddInput(HTTPCookieJoinActionInputOldCookies, oldCookiesIn)
+	action.AddInput(HTTPCookieJoinActionInputNewCookies, newCookiesIn)
+	action.AddOutput(HTTPCookieJoinActionOutputUpdatedCookies, updatedCookiesOut)
+
+	err := action.Run()
+
+	assert.Nil(t, err)
+
+	gotUpdatedCookies, ok := updatedCookiesOut.Remove().(map[string]string)
+
+	assert.True(t, ok)
+	assert.Equal(t, updatedCookies, gotUpdatedCookies)
+}
