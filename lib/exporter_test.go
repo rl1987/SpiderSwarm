c00ca895cc362048f540965f43e528ae7fb62eca
rl1987 <rl1987@users.noreply.github.com>
2021-08-21 17:20:33 +0300

    Write a basic unit-test for Exporter

---
+++ lib/exporter_test.go
@@ -0,0 +1,38 @@
+package spiderswarm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestExporterBackend struct {
+	AbstractExporterBackend
+	Items []*Item
+}
+
+func (teb *TestExporterBackend) WriteItem(item *Item) error {
+	teb.Items = append(teb.Items, item)
+	return nil
+}
+
+func TestExporterSimple(t *testing.T) {
+	backend := &TestExporterBackend{
+		Items: []*Item{},
+	}
+
+	exporter := NewExporter()
+
+	exporter.AddBackend(backend)
+
+	go exporter.Run()
+
+	testItem := NewItem("testItem", "testWorkflow", "B927B203-5A25-44DA-AABB-3D2A41085B3F", "638585AD-8280-4990-8CDD-E8CFB6788D10")
+
+	exporter.ItemsIn <- *testItem
+
+	close(exporter.ItemsIn)
+
+	assert.Equal(t, 1, len(backend.Items))
+	assert.Equal(t, testItem, backend.Items[0])
+}
