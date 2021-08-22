5e504df3ca9d2f5da83529161952d53a79d44310
rl1987 <rl1987@users.noreply.github.com>
2021-08-22 17:39:46 +0300

    Write constructor for SQLiteSpiderBusBackend (WIP)

--- lib/sqlitespiderbusbackend.go
+++ lib/sqlitespiderbusbackend.go
@@ -1,5 +1,47 @@
 package spiderswarm
 
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+	log "github.com/sirupsen/logrus"
+)
+
 type SQLiteSpiderBusBackend struct {
 	SpiderBusBackend
+
+	dbConn *sql.DB
+}
+
+func NewSQLiteSpiderBusBackend(sqliteFilePath string) *SQLiteSpiderBusBackend {
+	if sqliteFilePath == "" {
+		sqliteDirPath, err := ioutil.TempDir(os.TempDir(), "spiderbus_")
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed to create temp dir for SQLiteSpiderBusBackend: %v", err))
+			return nil
+		}
+
+		sqliteFilePath = sqliteDirPath + "/" + "spiderbus.db"
+	}
+
+	dbConn, err := sql.Open("sqlite3", sqliteFilePath)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to open DB connection: %v", err))
+		return nil
+	}
+
+	tx, _ := dbConn.Begin()
+
+	tx.Exec("CREATE TABLE IF NOT EXISTS scheduledTasks (id INTEGER PRIMARY KEY AUTOINCREMENT, raw BLOB)")
+	tx.Exec("CREATE TABLE IF NOT EXISTS taskPromises (id INTEGER PRIMARY KEY AUTOINCREMENT, raw BLOB)")
+	tx.Exec("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, raw BLOB)")
+
+	tx.Commit()
+
+	return &SQLiteSpiderBusBackend{
+		dbConn: dbConn,
+	}
 }
