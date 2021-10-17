70456ca6cde9f47826187f570d92b176db882ff5
rl1987 <rl1987@users.noreply.github.com>
2021-10-17 15:38:20 +0300

    Try reusing code from SQLiteSpiderBusBackend to implement MySQLSpiderBusBackend

--- lib/mysqlspiderbusbackend.go
+++ lib/mysqlspiderbusbackend.go
@@ -1,7 +1,211 @@
 package spsw
 
-import ()
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 type MySQLSpiderBusBackend struct {
 	SpiderBusBackend
+
+	dbConn *sql.DB
+	dsn    string
+}
+
+func NewMySQLSpiderBusBackend(dsn string) *MySQLSpiderBusBackend {
+	dbConn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	dbConn.SetConnMaxLifetime(time.Minute * 3)
+	dbConn.SetMaxOpenConns(10)
+	dbConn.SetMaxIdleConns(10)
+
+	tx, _ := dbConn.Begin()
+
+	tx.Exec("CREATE TABLE IF NOT EXISTS scheduledTasks (id INTEGER PRIMARY KEY AUTOINCREMENT, raw TEXT)")
+	tx.Exec("CREATE TABLE IF NOT EXISTS taskPromises (id INTEGER PRIMARY KEY AUTOINCREMENT, raw TEXT)")
+	tx.Exec("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, raw TEXT)")
+
+	tx.Commit()
+
+	return &MySQLSpiderBusBackend{
+		dbConn: dbConn,
+		dsn:    dsn,
+	}
+}
+
+func encodeEntry(entry interface{}) []byte {
+	buffer := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(buffer)
+
+	encoder.Encode(entry)
+
+	bytes, _ := ioutil.ReadAll(buffer)
+
+	return bytes
+}
+
+func decodeEntry(raw []byte, entry interface{}) interface{} {
+	buffer := bytes.NewBuffer(raw)
+	decoder := json.NewDecoder(buffer)
+
+	err := decoder.Decode(entry)
+	if err != nil {
+		spew.Dump(err)
+	}
+
+	return &entry
+}
+
+func (msbb *MySQLSpiderBusBackend) maybePrintError(err error) {
+	if err != nil && err != sql.ErrNoRows {
+		spew.Dump(err)
+	}
+}
+
+func (msbb *MySQLSpiderBusBackend) SendScheduledTask(scheduledTask *ScheduledTask) error {
+	raw := encodeEntry(scheduledTask)
+
+	tx, _ := msbb.dbConn.Begin()
+
+	tx.Exec("INSERT INTO scheduledTasks (raw) VALUES (?)", raw)
+
+	tx.Commit()
+
+	return nil
+}
+
+func (msbb *MySQLSpiderBusBackend) ReceiveScheduledTask() *ScheduledTask {
+	tx, _ := msbb.dbConn.Begin()
+
+	var row_id int
+	var raw []byte
+
+	row := tx.QueryRow("SELECT * FROM scheduledTasks ORDER BY id ASC LIMIT 1")
+
+	err := row.Scan(&row_id, &raw)
+	if err != nil {
+		tx.Rollback()
+		msbb.maybePrintError(err)
+		return nil
+	}
+
+	scheduledTask := &ScheduledTask{}
+
+	decodeEntry(raw, scheduledTask)
+
+	tx.Exec(fmt.Sprintf("DELETE FROM scheduledTasks WHERE id=%d", row_id))
+	tx.Commit()
+
+	return scheduledTask
+}
+
+func (msbb *MySQLSpiderBusBackend) SendTaskPromise(taskPromise *TaskPromise) error {
+	raw := encodeEntry(taskPromise)
+
+	tx, _ := msbb.dbConn.Begin()
+
+	tx.Exec("INSERT INTO taskPromises (raw) VALUES (?)", raw)
+
+	tx.Commit()
+
+	return nil
+}
+
+func (msbb *MySQLSpiderBusBackend) ReceiveTaskPromise() *TaskPromise {
+	tx, _ := msbb.dbConn.Begin()
+
+	var row_id int
+	var raw []byte
+
+	row := tx.QueryRow("SELECT * FROM taskPromises ORDER BY id ASC LIMIT 1")
+
+	err := row.Scan(&row_id, &raw)
+	if err != nil {
+		tx.Rollback()
+		msbb.maybePrintError(err)
+		return nil
+	}
+
+	taskPromise := &TaskPromise{}
+
+	decodeEntry(raw, taskPromise)
+
+	tx.Exec(fmt.Sprintf("DELETE FROM taskPromises WHERE id=%d", row_id))
+	tx.Commit()
+
+	return taskPromise
+}
+
+func (msbb *MySQLSpiderBusBackend) SendItem(item *Item) error {
+	raw := encodeEntry(item)
+
+	tx, _ := msbb.dbConn.Begin()
+
+	tx.Exec("INSERT INTO items (raw) VALUES (?)", raw)
+
+	tx.Commit()
+
+	return nil
+}
+
+func (msbb *MySQLSpiderBusBackend) ReceiveItem() *Item {
+	tx, _ := msbb.dbConn.Begin()
+
+	var row_id int
+	var raw []byte
+
+	row := tx.QueryRow("SELECT * FROM items ORDER BY id ASC LIMIT 1")
+
+	err := row.Scan(&row_id, &raw)
+	if err != nil {
+		tx.Rollback()
+		msbb.maybePrintError(err)
+		return nil
+	}
+
+	item := &Item{}
+
+	decodeEntry(raw, item)
+
+	tx.Exec(fmt.Sprintf("DELETE FROM items WHERE id=%d", row_id))
+	tx.Commit()
+
+	return item
+}
+
+func (msbb *MySQLSpiderBusBackend) getCountForTable(tableName string) int {
+	var output string
+
+	query, _ := msbb.dbConn.Prepare(fmt.Sprintf("SELECT COUNT(*) FROM %s;", tableName))
+
+	defer query.Close()
+
+	query.QueryRow().Scan(&output)
+
+	count, _ := strconv.Atoi(output)
+
+	return count
+}
+
+func (msbb *MySQLSpiderBusBackend) IsEmpty() bool {
+	nScheduledTasks := msbb.getCountForTable("scheduledTasks")
+	nPromises := msbb.getCountForTable("taskPromises")
+	nItems := msbb.getCountForTable("items")
+
+	return nScheduledTasks == 0 && nPromises == 0 && nItems == 0
+}
+
+func (msbb *MySQLSpiderBusBackend) Close() {
+	msbb.dbConn.Close()
 }
