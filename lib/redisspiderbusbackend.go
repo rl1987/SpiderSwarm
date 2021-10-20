03027dd2eacc58b555dfd79e4269ec4bfb977091
rl1987 <rl1987@users.noreply.github.com>
2021-10-20 13:17:41 +0300

    Write constructor and stubs for RedisSpiderBusBackend

--- lib/redisspiderbusbackend.go
+++ lib/redisspiderbusbackend.go
@@ -1,3 +1,58 @@
 package spsw
 
-import ()
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisSpiderBusBackend struct {
+	SpiderBusBackend
+
+	ctx         context.Context
+	serverAddr  string
+	redisClient *redis.Client
+}
+
+func NewRedisSpiderBusBackend(serverAddr string, password string) *RedisSpiderBusBackend {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     serverAddr,
+		Password: password,
+		DB:       0,
+	})
+
+	ctx := context.Background()
+
+	return &RedisSpiderBusBackend{
+		ctx:         ctx,
+		serverAddr:  serverAddr,
+		redisClient: redisClient,
+	}
+}
+
+func (rsbb *RedisSpiderBusBackend) SendScheduledTask(scheduledTask *ScheduledTask) error {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) ReceiveScheduledTask() *ScheduledTask {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) SendTaskPromise(taskPromise *TaskPromise) error {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) ReceiveTaskPromise() *TaskPromise {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) SendItem(item *Item) error {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) ReceiveItem() *Item {
+	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) Close() {
+}
