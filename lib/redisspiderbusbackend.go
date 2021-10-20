1b67831264de401d9cc5991a2375fd4e09ae2d86
rl1987 <rl1987@users.noreply.github.com>
2021-10-20 16:36:56 +0300

    Preliminary impl of RedisSpiderBusBackend

--- lib/redisspiderbusbackend.go
+++ lib/redisspiderbusbackend.go
@@ -2,6 +2,7 @@ package spsw
 
 import (
 	"context"
+	"errors"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-redis/redis/v8"
@@ -28,12 +29,9 @@ func NewRedisSpiderBusBackend(serverAddr string, password string) *RedisSpiderBu
 
 	consumerId := uuid.New().String()
 
-	status := redisClient.XGroupCreateMkStream(ctx, "items", consumerId, "$")
-	spew.Dump(status)
-	status = redisClient.XGroupCreateMkStream(ctx, "task_promises", consumerId, "$")
-	spew.Dump(status)
-	status = redisClient.XGroupCreateMkStream(ctx, "scheduled_tasks", consumerId, "$")
-	spew.Dump(status)
+	redisClient.XGroupCreateMkStream(ctx, "items", consumerId, "$")
+	redisClient.XGroupCreateMkStream(ctx, "task_promises", consumerId, "$")
+	redisClient.XGroupCreateMkStream(ctx, "scheduled_tasks", consumerId, "$")
 
 	return &RedisSpiderBusBackend{
 		ctx:         ctx,
@@ -44,29 +42,128 @@ func NewRedisSpiderBusBackend(serverAddr string, password string) *RedisSpiderBu
 }
 
 func (rsbb *RedisSpiderBusBackend) SendScheduledTask(scheduledTask *ScheduledTask) error {
+	raw := scheduledTask.EncodeToJSON()
+
+	err := rsbb.redisClient.XAdd(rsbb.ctx, &redis.XAddArgs{
+		Stream: "scheduled_tasks",
+		ID:     "$",
+		Values: map[string]interface{}{
+			"raw": raw,
+		},
+	}).Err()
+
+	if err != nil {
+		spew.Dump(err)
+		return err
+	}
+
 	return nil
+}
+
+func (rsbb *RedisSpiderBusBackend) readRawMessageFromStream(stream string) ([]byte, error) {
+	resp := rsbb.redisClient.XReadGroup(rsbb.ctx, &redis.XReadGroupArgs{
+		Group:    rsbb.consumerId,
+		Consumer: rsbb.consumerId,
+		Streams:  []string{stream},
+		Count:    1,
+		Block:    0,
+		NoAck:    false,
+	})
+
+	s, err := resp.Result()
+	if err != nil {
+		spew.Dump(err)
+		return nil, err
+	}
+
+	if len(s) == 1 && len(s[0].Messages) == 1 {
+		msg := s[0].Messages[0]
+		if raw, ok := msg.Values["raw"].([]byte); ok {
+			rsbb.redisClient.XAck(rsbb.ctx, stream, rsbb.consumerId, msg.ID)
+
+			return raw, nil
+		}
+	}
+
+	return nil, errors.New("Unknown error")
 }
 
 func (rsbb *RedisSpiderBusBackend) ReceiveScheduledTask() *ScheduledTask {
-	return nil
+	raw, err := rsbb.readRawMessageFromStream("scheduled_tasks")
+	if err != nil {
+		return nil
+	}
+
+	scheduledTask := NewScheduledTaskFromJSON(raw)
+
+	return scheduledTask
 }
 
 func (rsbb *RedisSpiderBusBackend) SendTaskPromise(taskPromise *TaskPromise) error {
+	raw := taskPromise.EncodeToJSON()
+
+	err := rsbb.redisClient.XAdd(rsbb.ctx, &redis.XAddArgs{
+		Stream: "task_promises",
+		ID:     "$",
+		Values: map[string]interface{}{
+			"raw": raw,
+		},
+	}).Err()
+
+	if err != nil {
+		spew.Dump(err)
+		return err
+	}
+
 	return nil
 }
 
 func (rsbb *RedisSpiderBusBackend) ReceiveTaskPromise() *TaskPromise {
-	return nil
+	raw, err := rsbb.readRawMessageFromStream("task_promises")
+	if err != nil {
+		return nil
+	}
+
+	taskPromise := NewTaskPromiseFromJSON(raw)
+
+	return taskPromise
 }
 
 func (rsbb *RedisSpiderBusBackend) SendItem(item *Item) error {
+	raw := item.EncodeToJSON()
+
+	err := rsbb.redisClient.XAdd(rsbb.ctx, &redis.XAddArgs{
+		Stream: "items",
+		ID:     "$",
+		Values: map[string]interface{}{
+			"raw": raw,
+		},
+	}).Err()
+
+	if err != nil {
+		spew.Dump(err)
+		return err
+	}
+
 	return nil
 }
 
 func (rsbb *RedisSpiderBusBackend) ReceiveItem() *Item {
-	return nil
+	raw, err := rsbb.readRawMessageFromStream("items")
+	if err != nil {
+		return nil
+	}
+
+	item := NewItemFromJSON(raw)
+
+	return item
 }
 
 func (rsbb *RedisSpiderBusBackend) Close() {
+	for _, stream := range []string{"items", "scheduled_tasks", "task_promises"} {
+		rsbb.redisClient.XGroupDelConsumer(rsbb.ctx, stream, rsbb.consumerId, rsbb.consumerId)
+		rsbb.redisClient.XGroupDestroy(rsbb.ctx, stream, rsbb.consumerId)
+	}
+
 	rsbb.redisClient.Close()
 }
