2f8c12c7efc4b9e41074d322d530bf2561b1b2c6
rl1987 <rl1987@users.noreply.github.com>
2021-11-20 15:05:50 +0200

    Introduce JSON support for TaskReport

--- lib/taskreport.go
+++ lib/taskreport.go
@@ -1,6 +1,10 @@
 package spsw
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+
 	"github.com/google/uuid"
 )
 
@@ -22,4 +26,29 @@ func NewTaskReport(jobUUID string, taskUUID string, taskName string, succeeded b
 		Succeeded: succeeded,
 		Error:     err,
 	}
+}
+
+func NewTaskReportFromJSON(raw []byte) *TaskReport {
+	taskReport := &TaskReport{}
+
+	buffer := bytes.NewBuffer(raw)
+	decoder := json.NewDecoder(buffer)
+
+	err := decoder.Decode(taskReport)
+	if err != nil {
+		return nil
+	}
+
+	return taskReport
+}
+
+func (tr *TaskReport) EncodeToJSON() []byte {
+	buffer := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(buffer)
+
+	encoder.Encode(tr)
+
+	bytes, _ := ioutil.ReadAll(buffer)
+
+	return bytes
 }
