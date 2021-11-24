800845ced37503b25ef9b71373c2e71185228890
rl1987 <rl1987@users.noreply.github.com>
2021-11-24 19:19:01 +0200

    Introduce TaskResult struct

---
+++ lib/taskresult.go
@@ -0,0 +1,15 @@
+package spsw
+
+import (
+	"github.com/google/uuid"
+)
+
+// TODO: use this instead of TaskReport
+type TaskResult struct {
+	UUID              string
+	JobUUID           string
+	TaskUUID          string
+	ScheduledTaskUUID string
+	Succeeded         bool
+	Error             error
+}
