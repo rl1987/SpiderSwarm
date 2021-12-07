205f30776032adf306326a802b1dea05293a6272
rl1987 <rl1987@users.noreply.github.com>
2021-12-07 18:09:42 +0200

    Add code for very basic web server to serve test data

---
+++ e2e_testing/basic_test_webserver/main.go
@@ -0,0 +1,19 @@
+// Code reused from: https://gist.github.com/paulmach/7271283
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+*port, nil))
+}
