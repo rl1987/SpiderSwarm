de3dfc8899e9012de2064bc39989806f29e79e7c
rl1987 <rl1987@users.noreply.github.com>
2021-12-13 16:14:57 +0200

    Preliminary implementation of JSONPathAction (WIP)

--- lib/jsonpathaction.go
+++ lib/jsonpathaction.go
@@ -1,7 +1,12 @@
 package spsw
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/google/uuid"
+	"github.com/ohler55/ojg/jp"
+	"github.com/ohler55/ojg/oj"
 )
 
 type JSONPathAction struct {
@@ -48,5 +53,72 @@ func NewJSONPathActionFromTemplate(actionTempl *ActionTemplate, workflowName str
 }
 
 func (jpa *JSONPathAction) Run() error {
+	if jpa.Inputs[JSONPathActionInputJSONStr] == nil && jpa.Inputs[JSONPathActionInputJSONBytes] == nil {
+		return errors.New("Input not connected")
+	}
+
+	if jpa.Outputs[JSONPathActionOutputStr] == nil {
+		return errors.New("Output not connected")
+	}
+
+	var jsonStr string
+
+	if jpa.Inputs[JSONPathActionInputJSONStr] != nil {
+		jsonStr = jpa.Inputs[JSONPathActionInputJSONStr].Remove().(string)
+	} else {
+		jsonBytes := jpa.Inputs[JSONPathActionInputJSONBytes].Remove().([]byte)
+		jsonStr = string(jsonBytes)
+	}
+
+	parsed, err := oj.ParseString(jsonStr)
+	if err != nil {
+		return err
+	}
+
+	query, err := jp.ParseString(jpa.JSONPath)
+	if err != nil {
+		return err
+	}
+
+	var result interface{}
+
+	result = query.Get(parsed)
+
+	if !jpa.Decode {
+		jsonStr2 := oj.JSON(result)
+
+		for _, outDP := range jpa.Outputs[JSONPathActionOutputStr] {
+			outDP.Add(jsonStr2)
+		}
+
+		return nil
+	}
+
+	if jpa.ExpectMany {
+		if resultStrings, okStrings := result.([]string); okStrings {
+			for _, outDP := range jpa.Outputs[JSONPathActionOutputStr] {
+				outDP.Add(resultStrings)
+			}
+		} else if resultIntfs, okIntfs := result.([]interface{}); okIntfs {
+			resultStrings := []string{}
+
+			for _, x := range resultIntfs {
+				resultStrings = append(resultStrings, fmt.Sprintf("%v", x))
+			}
+		}
+	} else {
+		if resultStr, okStr := result.(string); okStr {
+			for _, outDP := range jpa.Outputs[JSONPathActionOutputStr] {
+				outDP.Add(resultStr)
+			}
+		} else {
+			resultStr := fmt.Sprintf("%v", result)
+
+			for _, outDP := range jpa.Outputs[JSONPathActionOutputStr] {
+				outDP.Add(resultStr)
+			}
+		}
+	}
+
 	return nil
 }
