f3d9316da5e3dd922f561506166958aebd235d56
rl1987 <rl1987@users.noreply.github.com>
2021-12-13 17:59:23 +0200

    Unit-test more of JSONPathAction

--- lib/jsonpathaction_test.go
+++ lib/jsonpathaction_test.go
@@ -6,6 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const testJSONStr = `
+{
+  "firstName": "Nancy",
+  "lastName" : "Thompson",
+  "age"      : 17,
+  "address"  : {
+    "streetAddress": "1428 Elm Street",
+    "city"         : "Springwood, OH"
+  },
+  "phoneNumbers": [
+    {
+      "type"  : "iPhone",
+      "number": "0123-4567-8888"
+    },
+    {
+      "type"  : "home",
+      "number": "0123-4567-8910"
+    }
+  ]
+}
+`
+
 func TestNewJSONPathActionFromTemplate(t *testing.T) {
 	jsonPath := "$.store.book[*].author"
 	expectMany := true
@@ -60,4 +82,39 @@ func TestJSONPathActionRunBasic(t *testing.T) {
 	assert.True(t, ok)
 
 	assert.Equal(t, "John", resultStr)
+}
+
+func TestJSONPathActionRunExpectMany(t *testing.T) {
+	dataPipeIn := NewDataPipe()
+	dataPipeOut := NewDataPipe()
+
+	dataPipeIn.Add([]byte(testJSONStr))
+
+	action := NewJSONPathAction("$..number", true, true)
+
+	action.AddInput(JSONPathActionInputJSONBytes, dataPipeIn)
+	action.AddOutput(JSONPathActionOutputStr, dataPipeOut)
+
+	err := action.Run()
+	assert.Nil(t, err)
+
+	expectResults := []string{"0123-4567-8888", "0123-4567-8910"}
+
+	results, ok := dataPipeOut.Remove().([]string)
+	assert.True(t, ok)
+
+	assert.Equal(t, expectResults, results)
+
+	action.Decode = false
+	dataPipeIn.Add([]byte(testJSONStr))
+
+	err = action.Run()
+	assert.Nil(t, err)
+
+	result, ok2 := dataPipeOut.Remove().(string)
+	assert.True(t, ok2)
+
+	expectJSONStr := "[\"0123-4567-8888\",\"0123-4567-8910\"]"
+
+	assert.Equal(t, expectJSONStr, result)
 }
