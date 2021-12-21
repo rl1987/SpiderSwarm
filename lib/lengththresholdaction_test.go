380a0028cec321556b4ed90d06f0db5f50be8098
rl1987 <rl1987@users.noreply.github.com>
2021-12-21 17:22:28 +0200

    Unit-test LengthThresholdAction.Run()

--- lib/lengththresholdaction_test.go
+++ lib/lengththresholdaction_test.go
@@ -26,3 +26,32 @@ func TestNewLengthThresholdActionFromTemplate(t *testing.T) {
 	assert.Equal(t, threshold, action.Threshold)
 	assert.Equal(t, actionTempl.Name, action.Name)
 }
+
+func TestLengthThresholdActionRun(t *testing.T) {
+	threshold := 2
+
+	action := NewLengthThresholdAction(threshold)
+
+	dpIn := NewDataPipe()
+	action.AddInput(LengthThresholdActionInputSlice, dpIn)
+
+	dpOut := NewDataPipe()
+	action.AddOutput(LengthThresholdActionOutputThresholdUnmet, dpOut)
+
+	dpIn.Add([]string{"a"})
+
+	err := action.Run()
+	assert.Nil(t, err)
+
+	result, ok := dpOut.Remove().(bool)
+	assert.True(t, ok)
+	assert.True(t, result)
+
+	dpIn.Add([]string{"a", "b"})
+
+	action.Run()
+
+	result, ok = dpOut.Remove().(bool)
+	assert.True(t, ok)
+	assert.False(t, result)
+}
