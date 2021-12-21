d26bea32f01933155b38ba682a5d30ecebd2bfe9
rl1987 <rl1987@users.noreply.github.com>
2021-12-21 18:57:02 +0200

    Unit-test other two constructor functions for SpiderBusAdapter

--- lib/spiderbusadapter_test.go
+++ lib/spiderbusadapter_test.go
@@ -21,3 +21,41 @@ func TestNewSpiderBusAdapterForWorker(t *testing.T) {
 	assert.Nil(t, adapter.TaskPromisesOut)
 	assert.Nil(t, adapter.ItemsOut)
 }
+
+func TestNewSpiderBusAdapterForExporter(t *testing.T) {
+	spiderBus := NewSpiderBus()
+	exporter := NewExporter()
+
+	adapter := NewSpiderBusAdapterForExporter(spiderBus, exporter)
+
+	assert.NotNil(t, adapter)
+
+	assert.Equal(t, spiderBus, adapter.Bus)
+	assert.Equal(t, exporter.ItemsIn, adapter.ItemsOut)
+	assert.Nil(t, adapter.ScheduledTasksIn)
+	assert.Nil(t, adapter.ScheduledTasksOut)
+	assert.Nil(t, adapter.TaskPromisesIn)
+	assert.Nil(t, adapter.TaskPromisesOut)
+	assert.Nil(t, adapter.TaskResultsIn)
+	assert.Nil(t, adapter.TaskResultsOut)
+	assert.Nil(t, adapter.ItemsIn)
+}
+
+func TestNewSpiderBusAdapterForManager(t *testing.T) {
+	spiderBus := NewSpiderBus()
+	manager := NewManager(nil)
+
+	adapter := NewSpiderBusAdapterForManager(spiderBus, manager)
+
+	assert.NotNil(t, adapter)
+
+	assert.Equal(t, spiderBus, adapter.Bus)
+	assert.Equal(t, manager.TaskPromisesIn, adapter.TaskPromisesOut)
+	assert.Equal(t, manager.TaskResultsIn, adapter.TaskResultsOut)
+	assert.Equal(t, manager.ScheduledTasksOut, adapter.ScheduledTasksIn)
+	assert.Equal(t, manager.ItemsOut, adapter.ItemsIn)
+	assert.Nil(t, adapter.TaskPromisesIn)
+	assert.Nil(t, adapter.ScheduledTasksOut)
+	assert.Nil(t, adapter.TaskResultsIn)
+	assert.Nil(t, adapter.ItemsOut)
+}
