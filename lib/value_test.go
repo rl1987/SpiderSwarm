bcd91cbbb13462a2b5308b4862c4b8734637d759
rl1987 <rl1987@users.noreply.github.com>
2022-01-23 17:20:58 +0200

    Unit-test NewValue()

--- lib/value_test.go
+++ lib/value_test.go
@@ -1,6 +1,7 @@
 package spsw
 
 import (
+  	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +27,27 @@ func TestNewValueFromBool(t *testing.T) {
 	value := NewValueFromBool(true)
 
 	assert.Equal(t, expectValue, value)
+}
+
+func TestNewValue(t *testing.T) {
+	table := []struct{
+		Input interface{}
+		ExpectedValue *Value
+	}{
+		{42, &Value{ValueType: ValueTypeInt, IntValue: 42}},
+		{false, &Value{ValueType: ValueTypeBool, BoolValue: false}},
+		{"s", &Value{ValueType: ValueTypeString, StringValue: "s"}},
+		{[]string{"a", "b"}, &Value{ValueType: ValueTypeStrings, StringsValue: []string{"a", "b"}}},
+		{map[string]string{"a":"b"}, &Value{ValueType: ValueTypeMapStringToString, MapStringToStringValue: map[string]string{"a": "b"}}},
+		{map[string][]string{"x": []string{"1", "2"}}, &Value{ValueType: ValueTypeMapStringToStrings, MapStringToStringsValue: map[string][]string{"x": []string{"1", "2"}}}},
+		{[]byte("\xde\xea\xbe\xef"), &Value{ValueType: ValueTypeBytes, BytesValue: []byte("\xde\xea\xbe\xef")}},
+		{http.Header{}, &Value{ValueType: ValueTypeHTTPHeaders, HTTPHeadersValue: http.Header{}}},
+		{4.2, nil},
+	}
+
+	for _, entry := range table {
+		value := NewValue(entry.Input)
+		assert.Equal(t, entry.ExpectedValue, value)
+	}
+
 }
