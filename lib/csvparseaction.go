74399bcec36d05b36a4eebe4e23b3eda05639877
rl1987 <rl1987@users.noreply.github.com>
2022-02-01 17:47:54 +0200

    Write constructor functions for CSVParseAction

--- lib/csvparseaction.go
+++ lib/csvparseaction.go
@@ -1,6 +1,35 @@
 package spsw
 
 import (
-	//"github.com/google/uuid"
+	"github.com/google/uuid"
 )
+
+const CSVParseActionInputCSVBytes = "CSVParseActionInputCSVBytes"
+const CSVParseActionInputCSVStr = "CSVParseActionInputCSVStr"
+const CSVParseActionOutputMap = "CSVParseActionOutputMap"
+
+type CSVParseAction struct {
+	AbstractAction
+}
+
+func NewCSVParseAction() *CSVParseAction {
+	return &CSVParseAction{
+		AbstractAction: AbstractAction{
+			AllowedInputNames: []string{CSVParseActionInputCSVBytes, CSVParseActionInputCSVStr},
+			AllowedOutputNames: []string{CSVParseActionOutputMap},
+			Inputs: map[string]*DataPipe{},
+			Outputs: map[string][]*DataPipe{},
+			CanFail: false,
+			UUID: uuid.New().String(),
+		},
+	}
+}
+
+func NewCSVParseActionFromTemplate(actionTempl *ActionTemplate) Action {
+	action := NewCSVParseAction()
+
+	action.Name = actionTempl.Name
+
+	return action
+}
 
